da9e03fca80b58815982ea91d220cc715f0bc28f
j-ma <james@Qooways-Mac-mini-2.local>
2016-10-18 12:02:30 -0700

    [salesHistory] Added more table headers

--- app/service/mock-transactions.ts
+++ app/service/mock-transactions.ts
@@ -2,7 +2,7 @@ import { Transaction } from './transaction';
 
 export const TRANSACTIONS: Transaction[] = [
 	{
-		date: new Date('2015-12-16T16:16:00'),
+		date: new Date('2015-12-16T16:16:00-08:00'),
 		amount: 12.68,
 		points: 13,
 		reference: 4567894,
@@ -11,10 +11,82 @@ export const TRANSACTIONS: Transaction[] = [
 		source: 'Earned'
 	},
 	{
-		date: new Date('2015-12-16T16:12:00'),
+		date: new Date('2015-12-16T16:12:00-08:00'),
 		amount: 12.68,
 		points: 29,
 		reference: 123456,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-07T11:52:00-07:00'),
+		amount: 153.44,
+		points: 346,
+		reference: 65413,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-12T15:52:00-07:00'),
+		amount: 264.95,
+		points: 597,
+		reference: 321654,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-11T13:52:00-07:00'),
+		amount: 793.54,
+		points: 1786,
+		reference: 123451,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-11T13:52:00-07:00'),
+		amount: 49.86,
+		points: 113,
+		reference: 123459,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-23T13:52:00-07:00'),
+		amount: 8964.54,
+		points: 10000,
+		reference: 123458,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-23T13:52:00-07:00'),
+		amount: 23.67,
+		points: 54,
+		reference: 12457,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-23T13:50:00-07:00'),
+		amount: 83.22,
+		points: 188,
+		reference: 12456,
+		member: 'John Smith',
+		card: 8081306450415790,
+		source: 'Earned'
+	},
+	{
+		date: new Date('2015-09-23T13:50:00-07:00'),
+		amount: 64.53,
+		points: 146,
+		reference: 12456789,
 		member: 'John Smith',
 		card: 8081306450415790,
 		source: 'Earned'
